97a6647d771aec44f2d23ce4c05d3aaef0210bc6
Ban Cintia <bncincia@gmail.com>
2022-03-29 19:48:17 +0200

    login komponens létrehozva, formázva

---
+++ web/LifeStyle/src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(
+    private auth: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      pass: [''],
+    });
+  }
+
+  login() {
+    
+    let email = this.loginForm.value.email;
+    let pass = this.loginForm.value.pass;
+
+    this.auth.login(email, pass).subscribe((res) => {
+      console.log(res);
+      if (res.token) {
+        localStorage.setItem(
+          'currentUser',
+          JSON.stringify({ token: res.token, name: res.name })
+        );
+        this.router.navigate(['vehicle']);
+      } else {
+        alert('Hiba a belepes sikertelen');
+      }
+    });
+  }
+}
